3774a18f2e37e2937eba044f90a085332936e8a8
HampusJ <hampus.johannesson@utb.saffle.se>
2017-11-06 20:25:26 +0100

    Added super-basic networking

---
+++ Shady Game Studios first game/Assets/Scripts/NetworkController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkController : NetworkBehaviour {
+
+    public GameObject hostPlayerPrefab;
+    public GameObject remotePlayerPrefab;
+    
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public virtual void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+    {
+        var player = (GameObject)GameObject.Instantiate(remotePlayerPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        if (isServer)
+        {
+            player = (GameObject)GameObject.Instantiate(hostPlayerPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        }
+        NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+    }
+}
--- Shady Game Studios first game/Assets/Scripts/PlayerController.cs
+++ Shady Game Studios first game/Assets/Scripts/PlayerController.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : NetworkBehaviour {
 
 	// Use this for initialization
 	void Start ()
@@ -13,7 +14,10 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-
+        if (!isLocalPlayer)
+        {
+            return;
+        }
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * 300.0f;
         var z = Input.GetAxis("Vertical") * Time.deltaTime * 5.0f;
 
