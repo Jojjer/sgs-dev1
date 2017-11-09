fb9a9dead48f5968398b356a8a653ae20ab34f16
4ndr34s88 <andreas.danielsson@elev.saffle.se>
2017-11-09 20:23:05 +0100

    AI

---
+++ Shady Game Studios first game/Assets/Moveto1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Moveto1 : MonoBehaviour
+{
+
+    public Transform Enemy;
+
+    public Transform goal;
+
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Vector3 position = new Vector3(Random.Range(-25.0f, 25.0f), 2f, Random.Range(25.0f, 15.0f));
+
+            Instantiate(Enemy, position, Quaternion.identity);
+        }
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+
+    }
+}
---
+++ Shady Game Studios first game/Assets/Scripts/goal.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class goal : MonoBehaviour {
+
+    public Transform walktodoor;
+
+    // Use this for initialization
+    void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = /*new Vector3(-1, 2, -18);*/walktodoor.position;
+    }
+    
+	
+}
