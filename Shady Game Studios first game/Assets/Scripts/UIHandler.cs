e4c72940938221751cf0b3cf2eb6e2564805dd28
HampusJ <hampus.johannesson@utb.saffle.se>
2017-11-09 22:08:30 +0100

    Fixed spawn-system, kind of
    It works now, just not quite as intended

---
+++ Shady Game Studios first game/Assets/Scripts/UIHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class UIHandler : NetworkBehaviour {
+
+    public GameObject enemyprefab;
+
+    public InputField NumberOfEnemies;
+
+    private int enemyAmount;
+
+    [Command]
+    public void CmdButtonClick()
+    {
+        enemyAmount = int.Parse(NumberOfEnemies.text);
+        for (int i = 0; i < enemyAmount; i++)
+        {
+            Vector3 position = new Vector3(Random.Range(-25.0f, 25.0f), 2f, Random.Range(25.0f, 15.0f));
+            var enemy = (GameObject)Instantiate(enemyprefab, position, Quaternion.identity);
+            NetworkServer.Spawn(enemy);
+        }
+    }
+
+}
+
